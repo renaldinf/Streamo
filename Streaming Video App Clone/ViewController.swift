eec8da105f5bedbabdfbd6e3ebf20f5c3c0ec266
Renaldi <renaldinajwanfauzan@gmail.com>
2022-03-24 14:06:38 +0700

    Initial Commit

---
+++ Streaming Video App Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Streaming Video App Clone
+//
+//  Created by KTMacBook on 24/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
